daa992ec45fbd8e5f4e65bb78faa5c34199ce05c
ShubhangiSrivastava_qait <46716724+ShubhangiSrivastavaqait@users.noreply.github.com>
2019-01-18 10:09:26 +0530

    Update AreaShape.java

--- AreaShape.java
+++ AreaShape.java
@@ -1,12 +1,12 @@
-package myproject;
 
 	import java.util.*;
 	public class AreaShape
 	{
-	static double area = 0;
-	static double result = 0;
+	static double area = 0.0;
+	static double result = 0.0;
+        /* functions to calculate area of different shapes */
 	public static double areacircle(double radius){
-	area = 3.14 * radius * radius;
+	area = 3.14 * radius * radius; 
 	return area;
 	}
 	public static double areasquare(double side){
@@ -26,13 +26,14 @@ package myproject;
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Enter the input");
 	String str = sc.nextLine();
+	/* static method calling by class */	
 	if(str.equalsIgnoreCase("circle")){
 	System.out.println("Enter radius as input");
 	double r = sc.nextDouble();
 	result = AreaShape.areacircle(r);
 	System.out.println("Area calculated of the circle is" +result);
 	}
-	else if(str.equalsIgnoreCase("rectangle")){
+	else if(str.equalsIgnoreCase("rectangle")){ 
 	System.out.println("Enter the length and breadth as input");
 	double l = sc.nextDouble();
 	double b = sc.nextDouble();
@@ -43,13 +44,12 @@ package myproject;
 	double a = sc.nextDouble();
 	result = AreaShape.areasquare(a);
 	System.out.println("Area calculated of the rectangle is" +result);}
-	else if(str.equalsIgnoreCase("triangle")){
+	else if(str.equalsIgnoreCase("triangle")){ 
 	System.out.println("Enter the base and altitude as input");
 	double ba = sc.nextDouble();
 	double al = sc.nextDouble();
 	result = AreaShape.arearectangle(ba,al);
 	System.out.println("Area calculated of the triangle is" +result);}
-	else{
-	System.out.println("Enter the input correctly");}
+	else{ System.out.println("Enter the input correctly");} /*error message if no condition matches*/
 	}
 	}
